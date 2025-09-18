75d2c697295704f45ac0776e004c78cad1ab3a18
KUNALI BHAVARTHE <kunalibhavarthe2003@gmail.com>
2025-09-18 20:32:04 +0530

    Create swaping.-using-third-variable.c

---
+++ swaping.-using-third-variable.c
@@ -0,0 +1,13 @@
+void main(){
+int a,b,c;
+printf("enter two numbers\n");
+scanf("%d %d",&a,&b);
+
+printf("numbers before swaping %d %d\n",a,b);
+c=a;
+a=b;
+b=c;
+
+printf("numbers after swaping %d %d\n",a,b);
+getch();
+}
